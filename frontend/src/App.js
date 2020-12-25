21169d9bd8a38bd09b2f7dd82fdd6790b189eecd
Patti85 <pbeier24@googlemail.com>
2020-12-25 19:05:58 +0100

    file upload is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,26 @@ function App() {
   return (
     <div className="App">
       <Canvas></Canvas>
+      <img src="" height="200" alt="Image preview..."></img>
       <input type="file" id="imageUpload" accept=".png, .jpg, .jpeg"
-      onChange={(event)=> { alert("test")}}/>
+        onChange={(event) => { uploadImage() }} />
     </div>
   );
+  function uploadImage() { 
+    var preview = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+  }
 }
 
 export default App;
@@ -20,24 +36,22 @@ function Canvas(props) {
     alert("clientX: " + event.clientX + " - clientY: " + event.clientY);
   }
 
-  function uploadImage(event){
-   // printMousePos(event);
+  function uploadImage(event) {
+    // printMousePos(event);
     var marker = document.createElement("div");
     marker.style.position = 'absolute';
-    marker.style.backgroundColor = "red";  
+    marker.style.backgroundColor = "red";
     marker.style.width = '5px';
     marker.style.height = '5px';
     marker.style.borderRadius = '50%'
     marker.style.top = event.clientY + 'px';
     marker.style.left = event.clientX + 'px';
     document.getElementById('canvas').appendChild(marker)
-
-     
   }
 
   return (
-    <div id='canvas' onClick={uploadImage} style={{ backgroundColor: 'black', width: '800px', height: '800px' }}>
-     </div>
+    <div id='canvas' style={{ backgroundColor: 'black', width: '800px', height: '800px' }}>
+    </div>
   );
 
 
